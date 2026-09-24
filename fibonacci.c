f9594349256e910962d2798de4265dc1bbc16790
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-24 11:00:52 +0530

    Add Fibonacci sequence generator
    Implement Fibonacci sequence generator in C.

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 0, b = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", a);
+
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+OUTPUT:
+Enter number of terms: 9
+0 1 1 2 3 5 8 13 21 
